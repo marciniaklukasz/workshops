3196ee2fe95914e77fbf478d25a63591450d9ad4
Łukasz Marciniak <lukaszmarciniakk@gmail.com>
2023-11-23 19:30:34 +0100

    20231123_1930

---
+++ pracadomowa/z4.py
@@ -0,0 +1,20 @@
+"""
+Zadanie 1.4
+Firma remontowa
+Firma remontowa posiada taki cennik usług:
+•
+gipsowanie ścian: 100 zł za metr kwadratowy ściany
+•
+malowanie ścian i sufitów: 30 zł za metr kwadratowy
+•
+położenie paneli podłogowych: 50 zł za metr kwadratowy podłogi
+•
+położenie listew przypodłogowych: 40 zł za metr bieżący
+Napisz program, który pomaga wycenić pracę na podstawie wymiarów pomieszczenia. Zakładając, że pomieszczenie ma kształt prostokąta, program powinien zapytać o dwa wymiary poziome (w metrach) oraz o wysokość i na tej podstawie obliczyć powierzchnię podłogi, sufitu oraz łącznie wszystkich ścian, a także obwód pokoju (listy podłogowe).
+Następnie pytaj użytkownika jakie elementy prac mają być wykonane. Na przykład pytanie "Czy gipsować ściany?", a użytkownik odpowiada T lub N, i tak dla każdej pracy.
+Na końcu, w zależności od podanych wymiarów pomieszczenia oraz wybranych prac, podawana jest łączna suma do zapłaty.
+"""
+
+print("Gipsowanie scian: 100 zl za metr kwadratowy")
+print("Malowanie ścian i sufitów: 30 zl za metr kwadratowy")
+print("Położenie paneli podłogowych: 100 zl za metr kwadratowy")
